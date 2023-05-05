baf5cfc4d5a7c86f2b6f573df98fed8e6a09760c
Vladyslava Korogodova <korogodova.vladislava@gmail.com>
2023-05-05 13:08:42 +0300

    Add tasks 6

--- tasks-6/task-4.js
+++ tasks-6/task-4.js
@@ -2,10 +2,12 @@
 const submitBtn = document.querySelector('.btn');
 
 submitBtn.addEventListener('click', (e) => {
+    document.querySelector('.out').innerHTML = '';
     const { value: name } = document.querySelector('.text-data.arr');
     const { value: phone } = document.querySelector('input[name="phone"]');
     const { value: birthday } = document.querySelector('input[name="birthday"]');
     const { value: email } = document.querySelector('input[name="email"]');
 
-    alert(`Your name: ${name}, your phone: ${phone}, your birthday: ${birthday}, your email: ${email}.`);
+    document.querySelector('.out').innerHTML = `Your name: ${name}, your phone: ${phone}, your birthday: ${birthday}, your email: ${email}.`;
+    
 }); 
